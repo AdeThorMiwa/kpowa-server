6abf744c4af4c0b2de7dfbaa232cc0869b69f4eb
AdeThorMiwa <bendamyth@gmail.com>
2023-09-26 10:34:17 +0100

    add a sample env file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use kpower_server::{app::Application, config::get_config};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv::dotenv().expect("Unable to load environment variables from .env file");
+    dotenv::dotenv().ok();
     let config = get_config().expect("Failed to read configuration.");
     Application::build(config).await?;
     Ok(())
